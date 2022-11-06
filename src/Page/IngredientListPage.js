4af7d9bfc68b38e3705b70401bdef22acaac70f3
JihamSong <90690578+JihamSong@users.noreply.github.com>
2022-11-06 16:56:59 +0900

    [Feat] Ingredient list page 1차 완성

--- src/Page/IngredientListPage.js
+++ src/Page/IngredientListPage.js
@@ -1,7 +1,178 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import '../scss/IngredientListPage.scss';
+import { HiPlusCircle, HiMinusCircle } from 'react-icons/hi';
+
+function LogoNav() {
+  return (
+    <div className='logo-nav-container'>
+      <div className='logo-container'>
+        <NavLink to='/orderlist'>
+          <img
+            className='MrDaebakLogo'
+            alt='MrDaebakLogo'
+            src='img/MrDaebakLogo.png'
+          />
+        </NavLink>
+      </div>
+      <div className='nav-container'>
+        <div className='nav-container-inner'>
+          <div className='nav-title'>주문목록</div>
+          <div>
+            <NavLink to='/orderlist'>예약</NavLink>
+          </div>
+          <div>
+            <NavLink to='/orderlist'>조리중</NavLink>
+          </div>
+          <div>
+            <NavLink to='/orderlist'>배달중</NavLink>
+          </div>
+          <div>
+            <NavLink to='/orderlist'>완료</NavLink>
+          </div>
+          <div className='nav-title nav-manage'>우리가게관리</div>
+          <div>
+            <NavLink to='/ingredientlist'>재고현황</NavLink>
+          </div>
+          <div>
+            <NavLink to='/ingredientorder'>발주관리</NavLink>
+          </div>
+          <div>
+            <NavLink to='/customerlist'>단골관리</NavLink>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 function IngredientListPage() {
-  return <div>This is Ingredient List Page</div>;
+  return (
+    <div className='layout-container'>
+      <LogoNav />
+      <div className='nexttonav'>
+        <div className='ingredientlist-container'>
+          <div className='title-container'>
+            <div className='title-main'>재고현황</div>
+            <div className='title-detail'>
+              어제 재고현황과 오늘 재고현황을 보여줍니다.<br></br>술 상품은
+              상점에서 구매한 수량만큼 당일입고 수량을 직접 변경할 수 있습니다
+            </div>
+          </div>
+          <div className='content-container'>
+            <table>
+              <thead>
+                <th>상품명</th>
+                <th>카테고리</th>
+                <th>전일재고</th>
+                <th>당일입고</th>
+                <th>당일출고</th>
+                <th>현재재고</th>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>스테이크</td>
+                  <td>식사</td>
+                  <td>10</td>
+                  <td>50</td>
+                  <td>30</td>
+                  <td>30</td>
+                </tr>
+                <tr>
+                  <td>샐러드</td>
+                  <td>식사</td>
+                  <td>10</td>
+                  <td>50</td>
+                  <td>30</td>
+                  <td>30</td>
+                </tr>
+                <tr>
+                  <td>에그스크램블</td>
+                  <td>식사</td>
+                  <td>10</td>
+                  <td>50</td>
+                  <td>30</td>
+                  <td>30</td>
+                </tr>
+                <tr>
+                  <td>와인</td>
+                  <td>술</td>
+                  <td>10</td>
+                  <td className='alcohol-arrived'>
+                    <div className='button'>
+                      <HiMinusCircle />
+                    </div>
+                    <span className='number'>50</span>
+                    <div className='button'>
+                      <HiPlusCircle />
+                    </div>
+                  </td>
+                  <td>30</td>
+                  <td>30</td>
+                </tr>
+                <tr>
+                  <td>샴페인</td>
+                  <td>술</td>
+                  <td>10</td>
+                  <td className='alcohol-arrived'>
+                    <div className='button'>
+                      <HiMinusCircle />
+                    </div>
+                    <span className='number'>50</span>
+                    <div className='button'>
+                      <HiPlusCircle />
+                    </div>
+                  </td>
+                  <td>30</td>
+                  <td>30</td>
+                </tr>
+                <tr>
+                  <td>베이컨</td>
+                  <td>식사</td>
+                  <td>10</td>
+                  <td>50</td>
+                  <td>30</td>
+                  <td>30</td>
+                </tr>
+                <tr>
+                  <td>베이컨</td>
+                  <td>식사</td>
+                  <td>10</td>
+                  <td>50</td>
+                  <td>30</td>
+                  <td>30</td>
+                </tr>
+                <tr>
+                  <td>베이컨</td>
+                  <td>식사</td>
+                  <td>10</td>
+                  <td>50</td>
+                  <td>30</td>
+                  <td>30</td>
+                </tr>
+                <tr>
+                  <td>베이컨</td>
+                  <td>식사</td>
+                  <td>10</td>
+                  <td>50</td>
+                  <td>30</td>
+                  <td>30</td>
+                </tr>
+                <tr>
+                  <td>베이컨</td>
+                  <td>식사</td>
+                  <td>10</td>
+                  <td>50</td>
+                  <td>30</td>
+                  <td>30</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default IngredientListPage;
