1674ba3b6ced3239befc915da162cbf09c518cd8
JihamSong <jeeham0226@gmail.com>
2022-11-08 00:40:22 +0900

    [Feat] Ingredient order page 1차 완성, [Refactor] Order list page 및 Ingredient list page 리팩토링

---
+++ src/Component/IngredientOrder/index.js
@@ -0,0 +1,105 @@
+import React from 'react';
+
+function IngredientOrder() {
+  return (
+    <div className='nexttonav'>
+      <div className='ingredientorder-container'>
+        <div className='title-container'>
+          <div className='title-main'>발주관리</div>
+          <div className='title-detail'>
+            발주를 신청하면 화요일과 목요일에 입고됩니다.
+          </div>
+        </div>
+        <div className='content-container'>
+          <div className='table-container'>
+            <table>
+              <thead>
+                <th>상품명</th>
+                <th>카테고리</th>
+                <th>단가</th>
+                <th>수량</th>
+                <th>총금액</th>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>스테이크</td>
+                  <td>식사</td>
+                  <td>20,000원</td>
+                  <td>50</td>
+                  <td>1,000,000원</td>
+                </tr>
+                <tr>
+                  <td>에그스크램블</td>
+                  <td>식사</td>
+                  <td>20,000원</td>
+                  <td>50</td>
+                  <td>1,000,000원</td>
+                </tr>
+                <tr>
+                  <td>베이컨</td>
+                  <td>식사</td>
+                  <td>20,000원</td>
+                  <td>50</td>
+                  <td>1,000,000원</td>
+                </tr>
+                <tr>
+                  <td>스테이크</td>
+                  <td>식사</td>
+                  <td>20,000원</td>
+                  <td>50</td>
+                  <td>1,000,000원</td>
+                </tr>
+                <tr>
+                  <td>에그스크램블</td>
+                  <td>식사</td>
+                  <td>20,000원</td>
+                  <td>50</td>
+                  <td>1,000,000원</td>
+                </tr>
+                <tr>
+                  <td>베이컨</td>
+                  <td>식사</td>
+                  <td>20,000원</td>
+                  <td>50</td>
+                  <td>1,000,000원</td>
+                </tr>
+                <tr>
+                  <td>스테이크</td>
+                  <td>식사</td>
+                  <td>20,000원</td>
+                  <td>50</td>
+                  <td>1,000,000원</td>
+                </tr>
+                <tr>
+                  <td>에그스크램블</td>
+                  <td>식사</td>
+                  <td>20,000원</td>
+                  <td>50</td>
+                  <td>1,000,000원</td>
+                </tr>
+                <tr>
+                  <td>베이컨</td>
+                  <td>식사</td>
+                  <td>20,000원</td>
+                  <td>50</td>
+                  <td>1,000,000원</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className='bottom-container'>
+            <div className='total-order-price'>
+              <span className='hangeul-text'>총 발주 금액</span>
+              <span className='number'>1,000,000원</span>
+            </div>
+            <div className='order-button-container'>
+              <div className='order-button'>발주하기</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default IngredientOrder;
--- src/Page/IngredientOrderPage.js
+++ src/Page/IngredientOrderPage.js
@@ -1,22 +1,8 @@
 import React from 'react';
 import Layout from '../Component/Layout';
 import LogoNav from '../Component/LogoNav';
+import IngredientOrder from '../Component/IngredientOrder';
 import '../scss/IngredientOrderPage.scss';
-
-function IngredientOrder() {
-  return (
-    <div className='nexttonav'>
-      <div className='ingredientorder-container'>
-        <div className='title-container'>
-          <div className='title-main'>발주관리</div>
-          <div className='title-detail'>
-            발주를 신청하면 화요일과 목요일에 입고됩니다.
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
 
 function IngredientOrderPage() {
   return (
