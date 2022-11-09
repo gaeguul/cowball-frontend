1280dfea773f239cb87d82914ea5a8179e71dbb9
JihamSong <jeeham0226@gmail.com>
2022-11-09 22:20:33 +0900

    [Refactor] 리팩토링

--- src/Component/IngredientOrder/OrderNumberButton.js
+++ src/Component/IngredientOrder/OrderNumberButton.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BiPlus, BiMinus } from 'react-icons/bi';
 
 function OrderNumberButton() {
+  const [orderNumber, setOrderNumber] = useState(0);
+
+  const decreaseOrderNumber = () => {
+    if (orderNumber == 0) {
+      console.log('더 이상 줄일 수 없습니다');
+    } else {
+      setOrderNumber(orderNumber - 1);
+    }
+  };
+
+  const increaseOrderNumber = () => {
+    setOrderNumber(orderNumber + 1);
+  };
+
   return (
     <div className='order-number-button-container'>
       <div className='order-number-button'>
         <div className='button-container'>
-          <BiMinus className='button' />
+          <BiMinus className='button' onClick={decreaseOrderNumber} />
         </div>
-        <div className='number'>10</div>
+        <div className='number'>{orderNumber}</div>
         <div className='button-container'>
-          <BiPlus className='button' />
+          <BiPlus className='button' onClick={increaseOrderNumber} />
         </div>
       </div>
     </div>
