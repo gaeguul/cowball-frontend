13fb42355d197297c232b7806b2006cadafc3683
JihamSong <jeeham0226@gmail.com>
2022-11-12 16:32:48 +0900

    [Feat] 각 페이지에 따른 scss 파일 추가

--- src/Page/CartPage.js
+++ src/Page/CartPage.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import CustomerLayout from '../Component/CustomerLayout';
+import Header from '../Component/Header';
+import '../scss/CartPage.scss';
 
 function CartPage() {
-  return <div>This is CartPage</div>;
+  return (
+    <CustomerLayout>
+      <Header />
+    </CustomerLayout>
+  );
 }
 
 export default CartPage;
--- src/Page/OrderPage.js
+++ src/Page/OrderPage.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import CustomerLayout from '../Component/CustomerLayout';
+import Header from '../Component/Header';
+import '../scss/OrderPage.scss';
 
 function OrderPage() {
-  return <div>This is order page</div>;
+  return (
+    <CustomerLayout>
+      <Header />
+    </CustomerLayout>
+  );
 }
 
 export default OrderPage;
