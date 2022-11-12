e922c868bfb4998c5c2e569c10f1163df42e297a
JihamSong <jeeham0226@gmail.com>
2022-11-13 02:51:44 +0900

    [Feat] CartComplete 페이지 마크업 완성

--- src/Page/CartCompletePage.js
+++ src/Page/CartCompletePage.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import CustomerLayout from '../Component/CustomerLayout';
+import Header from '../Component/Header';
+import '../scss/CartCompletePage.scss';
+import { BsCheckCircle } from 'react-icons/bs';
 
 function CartCompletePage() {
-  return <div>This is CartCompletePage</div>;
+  return (
+    <CustomerLayout>
+      <Header />
+      <div className='center-container'>
+        <div className='order-complete-container'>
+          <div className='top-title'>
+            <BsCheckCircle />
+            <span className='title-text'>주문이 완료되었습니다.</span>
+          </div>
+          <div className='order-info-container'>
+            <div className='info-title'>주문번호</div>
+            <div className='info-content'>202210101234</div>
+            <div className='info-title'>총 결제금액</div>
+            <div className='info-content'>75,000원</div>
+            <div className='info-title'>예약일시</div>
+            <div className='info-content'>2022.10.31 (월) 19:00</div>
+          </div>
+          <div className='goto-main-button-container'>
+            <Link to='/'>
+              <div className='goto-main-button'>메인으로 이동</div>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </CustomerLayout>
+  );
 }
 
 export default CartCompletePage;
