dca130c25334cd13eb4f9a527ee990bb81ffb1da
JihamSong <jeeham0226@gmail.com>
2022-11-13 21:04:45 +0900

    [Feat] 주문내역 페이지 마크업 완성

--- src/Page/MyOrderPage.js
+++ src/Page/MyOrderPage.js
@@ -1,11 +1,199 @@
 import React from 'react';
 import CustomerLayout from '../Component/CustomerLayout';
 import Header from '../Component/Header';
+import '../scss/MyOrderPage.scss';
+
+function MyOrderComponent() {
+  return (
+    <div className='myorder-layout-container'>
+      <div className='myorder-component-container'>
+        <div className='main-title'>주문내역</div>
+        <div className='myorder-list-container'>
+          <div className='single-order-container'>
+            <div className='left-container'>
+              <div className='left-top-title'>
+                <span>11월 27일 주문</span>
+                <span className='dot'>•</span>
+                <span className='order-state'>완료</span>
+              </div>
+              <div className='myorder-dinner'>
+                <div className='dinner-and-style-container'>
+                  <div className='dinner-title'>
+                    <div className='dinner-name'>프렌치 디너</div>
+                    <div className='steak-degree-container'>
+                      <div className='steak-degree-title'>레어</div>
+                    </div>
+                  </div>
+                  <div className='dinner-option'>
+                    - 에그스크램블 삭제 (-5,000원)
+                  </div>
+                  <div className='dinner-option'>
+                    + 레어 스테이크 1인분 추가 (20,000원)
+                  </div>
+                  <div className='dinner-option'>
+                    + 베이컨 1장 추가 (2,000원)
+                  </div>
+                  <div className='style-name'>심플 스타일 (10,000원)</div>
+                  <div className='style-option'>+ 접시 추가 (3,000원)</div>
+                </div>
+                <div className='dinner-number'>1</div>
+                <div className='dinner-price'>40,000원</div>
+              </div>
+
+              <div className='total-container'>
+                <div className='title'>주문금액</div>
+                <div className='total-price'>73,000원</div>
+              </div>
+            </div>
+            <div className='right-container'>
+              <div className='right-box'>
+                <div className='delivery-info-container'>
+                  <div className='title'>배달정보</div>
+                  <div className='content-container'>
+                    <div className='rsv-date-title content-title'>예약일시</div>
+                    <div className='rsv-date second-col'>
+                      2022.01.01 (월) 19:00
+                    </div>
+                    <div className='address-title content-title'>배송지</div>
+                    <div className='address-input-container second-col'>
+                      서울특별시 동대문구
+                    </div>
+                    <div className='request-title content-title'>요청사항</div>
+                    <div className='request-input-container second-col'>
+                      맛있게 해주세요 ^^
+                    </div>
+                    <div className='card-number-title content-title'>
+                      카드번호
+                    </div>
+                    <div className='card-number second-col'>
+                      1235-1235-1235-1235
+                    </div>
+                  </div>
+                </div>
+                <div className='payment-info-container'>
+                  <div className='title'>결제금액</div>
+                  <div className='content-container'>
+                    <div className='total-price-title content-title'>
+                      주문금액
+                    </div>
+                    <div className='total-price-number'>80,000원</div>
+                    <div className='discount-price-title content-title'>
+                      단골할인금액
+                    </div>
+                    <div className='discount-price-number'>- 5,000원</div>
+                    <div className='payment-price-title  content-title'>
+                      총 결제금액
+                    </div>
+                    <div className='payment-price-number '>75,000원</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='single-order-container'>
+            <div className='left-container'>
+              <div className='left-top-title'>
+                <span>11월 27일 주문</span>
+                <span className='dot'>•</span>
+                <span className='order-state'>배달중</span>
+              </div>
+              <div className='myorder-dinner'>
+                <div className='dinner-and-style-container'>
+                  <div className='dinner-title'>
+                    <div className='dinner-name'>프렌치 디너</div>
+                    <div className='steak-degree-container'>
+                      <div className='steak-degree-title'>레어</div>
+                    </div>
+                  </div>
+                  <div className='dinner-option'>
+                    - 에그스크램블 삭제 (-5,000원)
+                  </div>
+                  <div className='dinner-option'>
+                    + 레어 스테이크 1인분 추가 (20,000원)
+                  </div>
+                  <div className='dinner-option'>
+                    + 베이컨 1장 추가 (2,000원)
+                  </div>
+                  <div className='style-name'>심플 스타일 (10,000원)</div>
+                  <div className='style-option'>+ 접시 추가 (3,000원)</div>
+                </div>
+                <div className='dinner-number'>1</div>
+                <div className='dinner-price'>40,000원</div>
+              </div>
+              <div className='myorder-dinner'>
+                <div className='dinner-and-style-container'>
+                  <div className='dinner-title'>
+                    <div className='dinner-name'>프렌치 디너</div>
+                    <div className='steak-degree-container'>
+                      <div className='steak-degree-title'>미디움</div>
+                    </div>
+                  </div>
+                  <div className='style-name'>그랜드 스타일 (15,000원)</div>
+                </div>
+                <div className='dinner-number'>1</div>
+                <div className='dinner-price'>33,000원</div>
+              </div>
+              <div className='total-container'>
+                <div className='title'>주문금액</div>
+                <div className='total-price'>73,000원</div>
+              </div>
+            </div>
+            <div className='right-container'>
+              <div className='right-box'>
+                <div className='delivery-info-container'>
+                  <div className='title'>배달정보</div>
+                  <div className='content-container'>
+                    <div className='rsv-date-title content-title'>예약일시</div>
+                    <div className='rsv-date second-col'>
+                      2022.01.01 (월) 19:00
+                    </div>
+                    <div className='address-title content-title'>배송지</div>
+                    <div className='address-input-container second-col'>
+                      서울특별시 동대문구
+                    </div>
+                    <div className='request-title content-title'>요청사항</div>
+                    <div className='request-input-container second-col'>
+                      맛있게 해주세요 ^^
+                    </div>
+                    <div className='card-number-title content-title'>
+                      카드번호
+                    </div>
+                    <div className='card-number second-col'>
+                      1235-1235-1235-1235
+                    </div>
+                  </div>
+                </div>
+                <div className='payment-info-container'>
+                  <div className='title'>결제금액</div>
+                  <div className='content-container'>
+                    <div className='total-price-title content-title'>
+                      주문금액
+                    </div>
+                    <div className='total-price-number'>80,000원</div>
+                    <div className='discount-price-title content-title'>
+                      단골할인금액
+                    </div>
+                    <div className='discount-price-number'>- 5,000원</div>
+                    <div className='payment-price-title  content-title'>
+                      총 결제금액
+                    </div>
+                    <div className='payment-price-number '>75,000원</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 function MyOrderPage() {
   return (
     <CustomerLayout>
       <Header />
+      <MyOrderComponent />
     </CustomerLayout>
   );
 }
--- src/Page/OrderPage.js
+++ src/Page/OrderPage.js
@@ -1,12 +1,223 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CustomerLayout from '../Component/CustomerLayout';
 import Header from '../Component/Header';
+import { BiPlus, BiMinus } from 'react-icons/bi';
+
 import '../scss/OrderPage.scss';
+
+// import { useForm } from 'react-hook-form';
+
+function ChangeDinnerNumberButton() {
+  const [dinnerNumber, setDinnerNumber] = useState(0);
+
+  const decreaseDinnerNumber = () => {
+    if (dinnerNumber == 0) {
+      console.log('더 이상 줄일 수 없습니다');
+    } else {
+      setDinnerNumber(dinnerNumber - 1);
+    }
+  };
+
+  const increaseDinnerNumber = () => {
+    setDinnerNumber(dinnerNumber + 1);
+  };
+
+  return (
+    <div className='dinner-number-button-container'>
+      <div className='dinner-number-button'>
+        <div className='button-container'>
+          <BiMinus className='button' onClick={decreaseDinnerNumber} />
+        </div>
+        <div className='number'>{dinnerNumber}</div>
+        <div className='button-container'>
+          <BiPlus className='button' onClick={increaseDinnerNumber} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function ChangeOptionNumberButton() {
+  const [optionNumber, setOptionNumber] = useState(0);
+
+  const decreaseOptionNumber = () => {
+    if (optionNumber == 0) {
+      console.log('더 이상 줄일 수 없습니다');
+    } else {
+      setOptionNumber(optionNumber - 1);
+    }
+  };
+
+  const increaseOptiomNumber = () => {
+    setOptionNumber(optionNumber + 1);
+  };
+
+  return (
+    <div className='option-number-button-container'>
+      <div className='option-number-button'>
+        <div className='button-container'>
+          <BiMinus className='button' onClick={decreaseOptionNumber} />
+        </div>
+        <div className='number'>{optionNumber}</div>
+        <div className='button-container'>
+          <BiPlus className='button' onClick={increaseOptiomNumber} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function OrderContainer() {
+  return (
+    <div className='order-container'>
+      <div className='top-info-container'>
+        <div className='img-container'>
+          <img
+            className='steak-image'
+            alt='steak-image'
+            src='/img/steak2.png'
+          />
+        </div>
+        <div className='right-info-container'>
+          <div className='dinner-title-ko'>발렌타인 디너</div>
+          <div className='dinner-title-en'>Valentine Dinner</div>
+          <div className='dinner-detail'>
+            압도적인 크기와 풍미가 가득한 토마호크 스테이크와 셰프 가니시의 만남
+          </div>
+          <div className='dinner-price-title'>
+            <span className='dinner-price'>108,000</span>
+            <span>원</span>
+          </div>
+        </div>
+      </div>
+      <div className='bottom-info-container'>
+        <div className='bottom-left-container'>
+          <div className='menu-item-container'>
+            <div className='title-container'>
+              <div className='main-title'>메뉴 구성</div>
+              <div className='sub-title'>
+                구성 메뉴 삭제는 스테이크를 제외한 메뉴 중 한 가지만 가능합니다.
+              </div>
+            </div>
+            <div className='radio-container'>
+              <label>
+                <input type='radio' name='delete-menu' id='1' value='1' />
+                와인 1잔
+              </label>
+              <label>
+                <input type='radio' name='delete-menu' id='2' value='2' />
+                하트모양과 큐피드 접시
+              </label>
+              <label>
+                <input type='radio' name='delete-menu' id='3' value='3' />
+                냅킨
+              </label>
+            </div>
+          </div>
+          <div className='steak-degree-container'>
+            <div className='title-container'>
+              <div className='main-title'>스테이크 굽기 단계</div>
+              <div className='sub-title'>
+                스테이크 굽기 단계를 선택해주세요.
+              </div>
+            </div>
+            <div className='radio-container'>
+              <label>
+                <input type='radio' name='steak-degree' id='1' value='1' />
+                레어
+              </label>
+              <label>
+                <input type='radio' name='steak-degree' id='2' value='2' />
+                미디움레어
+              </label>
+              <label>
+                <input type='radio' name='steak-degree' id='3' value='3' />
+                미디움
+              </label>
+              <label>
+                <input type='radio' name='steak-degree' id='4' value='4' />
+                미디움웰
+              </label>
+              <label>
+                <input type='radio' name='steak-degree' id='5' value='5' />
+                웰던
+              </label>
+            </div>
+          </div>
+          <div className='select-style-container'>
+            <div className='title-container'>
+              <div className='main-title'>스타일 선택</div>
+              <div className='sub-title'>
+                심플 스타일은 상자 접시/일반 냅킨/플라스틱 쟁반, 그랜드 스타일은
+                도자기 접시/플라스틱 컵/흰색 면 냅킨/나무 쟁반, 디럭스 스타일은
+                은 쟁반/작은 꽃병/도자기 접시/린넨 냅킨으로 구성되어 있습니다.
+              </div>
+              <div className='radio-container'>
+                <label>
+                  <input type='radio' name='style' id='1' value='1' />
+                  심플 스타일
+                </label>
+                <label>
+                  <input type='radio' name='style' id='2' value='2' />
+                  그랜드 스타일
+                </label>
+                <label>
+                  <input type='radio' name='style' id='3' value='3' />
+                  디럭스 스타일
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='bottom-right-container'>
+          <div className='title-container'>
+            <div className='main-title'>추가할 디너 옵션</div>
+          </div>
+          <div className='dinner-option-list'>
+            <div className='dinner-option-container'>
+              <div className='dinner-option-name'>스테이크 레어 1인분</div>
+              <div className='dinner-option-price'>10,000</div>
+              <div className='dinner-option-button'>
+                <ChangeOptionNumberButton />
+              </div>
+              <div className='dinner-option-name'>
+                스테이크 미디움레어 1인분
+              </div>
+              <div className='dinner-option-price'>10,000</div>
+              <div className='dinner-option-button'>
+                <ChangeOptionNumberButton />
+              </div>
+              <div className='dinner-option-name'>에그스크램블 1인분</div>
+              <div className='dinner-option-price'>3,000</div>
+              <div className='dinner-option-button'>
+                <ChangeOptionNumberButton />
+              </div>
+            </div>
+          </div>
+          <div className='dinner-number-and-price-container'>
+            <div className='dinner-number-title title'>디너 수량</div>
+            <div className='dinner-number-button content'>
+              <ChangeDinnerNumberButton />
+            </div>
+            <div className='total-price-title title'>총 가격</div>
+            <div className='total-price-number content'>240,000원</div>
+          </div>
+          <div className='put-dinner-cart-button-container'>
+            <button className='put-dinner-cart-button' type='submit'>
+              장바구니에 담기
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 function OrderPage() {
   return (
     <CustomerLayout>
       <Header />
+      <OrderContainer />
     </CustomerLayout>
   );
 }
